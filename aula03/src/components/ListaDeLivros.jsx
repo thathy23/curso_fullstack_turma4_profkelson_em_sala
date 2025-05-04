3f6c9f0bd30804802eebace07751c38880a10f73
Tatiane <thathy23@gmail.com>
2025-05-04 16:21:12 -0300

    correção ListaDeLivros

--- aula03/src/components/ListaDeLivros.jsx
+++ aula03/src/components/ListaDeLivros.jsx
@@ -1,27 +1,29 @@
-// import React from 'react'
-// import Livro from './Livro'
+import React from 'react'
+import Livro from './Livro'
 
-// const ListaDeLivros = () => {
+const ListaDeLivros = () => {
 
-//     const livros = [
-//         {titulo: 'O Hobbit', autor: 'J.R.R. Tolkien'},
-//         {titulo: 'Dom Casmurro', autor: 'Machado de Assis'},
-//         {titulo: '1984', autor: 'George Orwell'}
+    const livros = [
+        {titulo: 'O Hobbit', autor: 'J.R.R. Tolkien'},
+        {titulo: 'Dom Casmurro', autor: 'Machado de Assis'},
+        {titulo: '1984', autor: 'George Orwell'}
+    ]
+    //Iterar item por item dessa array pra renderizar na tela com map
 
-//     ]
-//   return (
-//     <div>
-//         <h3>Lista de Livros</h3>
-//         {
-
-//             livros.map((livro, index) => (
-//                 <Livro 
+  return (
+    <div>
+        <h3>Lista de Livros</h3>
+        {/* abrir uma templete expression pra fazer o .map ,
+         vai mapear cada item da lista acima pra renderizar*/}
         
-//             ))
-//         }    
+        {
+            livros.map((livro, index) => (
+                <Livro key={index}titulo={livro.titulo} autor={livro.autor}/>
+            ))        
+        }    
 
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default ListaDeLivros
+export default ListaDeLivros
